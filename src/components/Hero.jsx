9e0bb82ea05f03c234e1d3f2a1b46579b7206c89
Legacy-Col <lynxlestley@gmail.com>
2025-09-28 11:53:54 +0100

    kaizen

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -68,7 +68,7 @@ const Hero = () => {
                       </motion.p>
                       <motion.a
                         variants={childVariants}                  
-                        href="c:\Users\KAIZEN\Documents\My Resume.docx" target="_blank" rel="noopenner noreferrer" download className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10">Download Resume
+                        href="file:///C:/Users/KAIZEN/Documents/My%20Resume.pdf" target="_blank" rel="noopenner noreferrer" download className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10">Download Resume
                       </motion.a>  
                     </motion.div>
               </div>
